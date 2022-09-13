e5bddaf27d8b6b01317b411be10feef9a6bb3592
Fernando Jimenez Pereyra <A01734609@tec.mx>
2022-09-13 16:15:11 -0500

    .

--- api/predictions.py
+++ api/predictions.py
@@ -1,6 +1,6 @@
 from joblib import load
 
-model = load('trainedModel.joblib')
+model = load('/home/alt9193/Documents/IA/IA_Backend/api/trainedModel.joblib')
 
 def makePrediction(X):
     y_pred = model.predict(X)
