102a3ca2124e8d81281bd751cbfc061738d1ce36
Fernando Jimenez Pereyra <A01734609@tec.mx>
2022-09-13 14:49:43 -0500

    columns names

--- api/api.py
+++ api/api.py
@@ -64,5 +64,5 @@ def id(id):
 #     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # serve(app, host='0.0.0.0', port=8080)
+    # app.run(debug=True)
+    serve(app, host='0.0.0.0', port=8080)
